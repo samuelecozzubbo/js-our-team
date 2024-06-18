584392bfbe927fbb5fe4532afa6823a30a68d3ec
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-18 15:30:56 +0200

    Fix: image links now work (Bonus 1)

--- js/script.js
+++ js/script.js
@@ -4,32 +4,32 @@ const members = [
     {
         "name": "Wayne Barnett",
         "role": "Founder & CEO",
-        "img": "wayne-barnett-founder-ceo.jpg",
+        "img": "img/wayne-barnett-founder-ceo.jpg",
     },
     {
         "name": "Angela",
         "role": "Chief Editor",
-        "img": "angela-caroll-chief-editor.jpg",
+        "img": "img/angela-caroll-chief-editor.jpg",
     },
     {
         "name": "Walter Gordon",
         "role": "Office Manager",
-        "img": "walter-gordon-office-manager.jpg",
+        "img": "img/walter-gordon-office-manager.jpg",
     },
     {
         "name": "Angela Lopez",
         "role": "Social Media Manager",
-        "img": "angela-caroll-chief-editor.jpg",
+        "img": "img/angela-caroll-chief-editor.jpg",
     },
     {
         "name": "Scott Estrada",
         "role": "Developer",
-        "img": "scott-estrada-developer.jpg",
+        "img": "img/scott-estrada-developer.jpg",
     },
     {
         "name": "Barbara Ramos",
         "role": "Graphic Designer",
-        "img": "barbara-ramos-graphic-designer.jpg",
+        "img": "img/barbara-ramos-graphic-designer.jpg",
     },
 ];
 // MILESTONE 1:
