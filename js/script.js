91b7081b37f1ed28174da01b7714644535bf0e10
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-18 14:59:58 +0200

    Build: created array with members

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+const members = [
+    {
+        "name": "Wayne Barnett",
+        "role": "Founder & CEO",
+        "img": "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        "name": "Angela",
+        "role": "Chief Editor",
+        "img": "angela-caroll-chief-editor.jpg",
+    },
+    {
+        "name": "Walter Gordon",
+        "role": "Office Manager",
+        "img": "walter-gordon-office-manager.jpg",
+    },
+    {
+        "name": "Angela Lopez",
+        "role": "Social Media Manager",
+        "img": "angela-caroll-chief-editor.jpg",
+    },
+    {
+        "name": "Scott Estrada",
+        "role": "Developer",
+        "img": "scott-estrada-developer.jpg",
+    },
+    {
+        "name": "Barbara Ramos",
+        "role": "Graphic Designer",
+        "img": "barbara-ramos-graphic-designer.jpg",
+    },
+];
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
